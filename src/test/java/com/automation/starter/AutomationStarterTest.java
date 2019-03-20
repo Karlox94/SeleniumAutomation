e2c7a2e43ed04edd79e0f548cf13a2fd0de62e7e
Carlos Salcedo <caasalce@bancolombia.com.co>
2019-03-20 17:05:20 -0500

    Firts commit

--- src/test/java/com/automation/starter/AutomationStarterTest.java
+++ src/test/java/com/automation/starter/AutomationStarterTest.java
@@ -2,6 +2,8 @@ package com.automation.starter;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,11 +11,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class AutomationStarterTest {
 
 	private WebDriver driver;
-	private WebElement input;
+	private WebElement inputEmail;
+	private WebElement inputPassword;
+	private WebElement inputPassword2;
+	private WebElement inputName;
+	private WebElement inputLastname;
+	private List<WebElement> radioButtonList;
+	private WebElement inputCountry;
+	private Select inputCountrySelect;
 	private WebElement inputSubmit;
 
 	@Before
@@ -24,24 +34,40 @@ public class AutomationStarterTest {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("file:///Users/carlos.salcedo/Downloads/responsive-registration-form/index.html");
+		
+		inputEmail = driver.findElement(By.id("email"));
+		inputPassword = driver.findElement(By.id("password"));
+		inputPassword2 = driver.findElement(By.id("password2"));
+		inputName = driver.findElement(By.id("name"));
+		inputLastname = driver.findElement(By.id("lastname"));
+		inputCountry = driver.findElement(By.id("countrySelect"));
+		inputCountrySelect = new Select(inputCountry);
+		inputSubmit = driver.findElement(By.id("registerButton"));
 	}
 	
 	
 	@Test
-	public void test() {
-		String keyword = "youtube";
+	public void test() throws InterruptedException {
+		inputEmail.sendKeys("ikarloxi@gmail.com");
+		inputPassword.sendKeys("123456");
+		inputPassword2.sendKeys("123456");
+		inputName.sendKeys("Carlos");
+		inputLastname.sendKeys("Salcedo");
+		// Thread.sleep(2000);
+		radioButtonList = driver.findElements((By.tagName("label")));
+		List<WebElement> radioList = radioButtonList;
+		radioList.get(0).click();
+		inputCountry.click();
+		inputCountrySelect.selectByIndex(2);
+		radioList.get(2).click();
 		
-		input = driver.findElement(By.name("q"));
-		inputSubmit = driver.findElement(By.name("btnK"));
-		input.sendKeys(keyword);
-		inputSubmit.submit();
-		
-		assertEquals(keyword + " - Buscar con Google", driver.getTitle());
+		assertEquals(inputPassword.getText(), inputPassword2.getText());
+		// inputSubmit.submit();
 	}
 	
 	@After
 	public void end() {
-		driver.quit();
+		// driver.quit();
 	}
 
 }
---
+++ src/test/java/com/automation/starter/OperationsTest.java
@@ -0,0 +1,60 @@
+package com.automation.starter;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OperationsTest {
+	
+	private WebDriver driver;
+	private WebElement inputNum;
+	private WebElement inputDen;
+	private WebElement result1;
+	private WebElement result2;
+	private WebElement opertaionButton;
+
+	@Before
+	public void setup() {
+
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+		this.driver = new ChromeDriver();
+		this.driver.get("https://es.calcuworld.com/calculadoras-matematicas/fracciones-a-decimales");
+
+		this.inputNum = this.driver.findElement(By.id("num"));
+		this.inputDen = this.driver.findElement(By.id("den"));
+		this.result1 = this.driver.findElement(By.id("result1"));
+		this.result2 = this.driver.findElement(By.id("result2"));
+		this.opertaionButton = this.driver.findElement(By.id("button"));
+
+		this.inputNum.clear();
+		this.inputDen.clear();
+	}
+	
+	
+	@Test
+	public void test() throws InterruptedException {
+		String num = "2";
+		String den = "4";
+		String frac = String.valueOf(Float.parseFloat(num) / Float.parseFloat(den));
+		String dec = num+"/"+den;
+		this.inputNum.sendKeys(num);
+		this.inputDen.sendKeys(den);
+		this.opertaionButton.click();
+		
+
+		assertEquals(frac, this.result1.getText());
+		assertEquals(dec, this.result2.getText());
+	}
+	
+	@After
+	public void end() {
+		// this.driver.quit();
+	}
+
+}
