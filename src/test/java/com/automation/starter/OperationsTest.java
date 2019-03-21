03d991b2844eb305815d641ed4b3f22a9c63f7ab
Carlos Salcedo <caasalce@bancolombia.com.co>
2019-03-21 11:13:40 -0500

    Take screenshot

--- src/test/java/com/automation/starter/OperationsTest.java
+++ src/test/java/com/automation/starter/OperationsTest.java
@@ -1,27 +1,35 @@
 package com.automation.starter;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OperationsTest {
-	
+
 	private WebDriver driver;
 	private WebElement inputNum;
 	private WebElement inputDen;
 	private WebElement result1;
 	private WebElement result2;
 	private WebElement opertaionButton;
+	private File src;
 
 	@Before
 	public void setup() {
 
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
 
 		this.driver = new ChromeDriver();
 		this.driver.get("https://es.calcuworld.com/calculadoras-matematicas/fracciones-a-decimales");
@@ -35,26 +43,37 @@ public class OperationsTest {
 		this.inputNum.clear();
 		this.inputDen.clear();
 	}
-	
-	
+
 	@Test
-	public void test() throws InterruptedException {
+	public void test() throws InterruptedException  {
 		String num = "2";
 		String den = "4";
 		String frac = String.valueOf(Float.parseFloat(num) / Float.parseFloat(den));
-		String dec = num+"/"+den;
+		String dec = num + "/" + den;
 		this.inputNum.sendKeys(num);
 		this.inputDen.sendKeys(den);
 		this.opertaionButton.click();
-		
 
-		assertEquals(frac, this.result1.getText());
-		assertEquals(dec, this.result2.getText());
+		assertEquals(dec, this.result1.getAttribute("value"));
+		assertEquals(frac, this.result2.getAttribute("value"));
+		
+		this.takeScreenshot();
 	}
-	
+
 	@After
 	public void end() {
-		// this.driver.quit();
+		this.driver.quit();
+	}
+
+	private void takeScreenshot() {
+		this.src = ((TakesScreenshot) this.driver).getScreenshotAs(OutputType.FILE);
+		try {
+			// now copy the screenshot to desired location using copyFile //method
+			FileUtils.copyFile(src, new File("/Users/carlos.salcedo/Desktop/capture-"+System.currentTimeMillis()+".jpg"));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+
+		}
 	}
 
 }
