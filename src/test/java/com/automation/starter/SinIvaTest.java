187e7c47d8371fe77a835405aa6959e6129fa784
Carlos Salcedo <caasalce@bancolombia.com.co>
2019-03-22 16:30:35 -0500

    Do assert

--- src/test/java/com/automation/starter/SinIvaTest.java
+++ src/test/java/com/automation/starter/SinIvaTest.java
@@ -1,6 +1,6 @@
 package com.automation.starter;
 
-import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,9 +9,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -24,6 +21,7 @@ public class SinIvaTest {
 	private WebElement selectTipo;
 	private WebElement buttonCalcular;
 	private Select dropdown;
+	private WebElement result;
 
 	// String path_to_extension =
 	// "C:\\Users\\Angie\\AppData\\Local\\Google\\Chrome\\User
@@ -41,27 +39,32 @@ public class SinIvaTest {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
 		// this.driver = new ChromeDriver(capabilities);
 		this.driver = new ChromeDriver();
-		
+
 		this.driver.get("https://es.calcuworld.com/calculadoras-financieras/calculadora-iva/");
 		driver.manage().window().maximize();
 
-		
 		this.buttonSinIva = this.driver.findElement(By.xpath("//ul[@class='nav nav-tabs']/li[2]"));
 		this.camponum = this.driver.findElement(By.xpath("//*[@id=\"profile\"]/form/div/div/input"));
 		this.selectTipo = this.driver.findElement(By.xpath("//*[@id=\"profile\"]/form/select"));
 		this.dropdown = new Select(this.selectTipo);
 		this.buttonCalcular = this.driver.findElement(By.xpath("//*[@id=\"profile\"]/form/button"));
 
-
 	}
 
 	@Test
 	public void testSinIva() throws InterruptedException {
+		String cantidadSinIvaEsperada = "61,98 €";
+		
 		this.buttonSinIva.click();
 		this.camponum.sendKeys("75");
-		this.dropdown.selectByIndex(1);
+		this.dropdown.selectByIndex(0);
 		// dropdown.selectByVisibleText("IVA General (21%)");
 		this.buttonCalcular.click();
+		Thread.sleep(2000);
+		this.result = this.driver.findElement(By.className("text-success"));
+		System.out.println(this.result.getText());
+		
+		assertEquals(cantidadSinIvaEsperada, this.result.getText());
 
 	}
 
