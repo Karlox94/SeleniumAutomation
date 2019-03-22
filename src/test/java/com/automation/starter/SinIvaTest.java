a2af8ed9ee09d43ceea8212326403f73a18a496e
Carlos Salcedo <caasalce@bancolombia.com.co>
2019-03-22 14:22:45 -0500

    Fix select elements no interactable

--- src/test/java/com/automation/starter/SinIvaTest.java
+++ src/test/java/com/automation/starter/SinIvaTest.java
@@ -1,56 +1,75 @@
 package com.automation.starter;
 
-import static org.junit.Assert.*;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-
-import net.bytebuddy.asm.Advice.This;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SinIvaTest {
 
-	
-
-	private ChromeDriver driver;
+	private WebDriver driver;
 	private WebElement buttonSinIva;
-	private WebElement camponum1;
+	private WebElement buttonConIva;
+	private WebElement camponum;
 	private WebElement selectTipo;
 	private WebElement buttonCalcular;
 	private Select dropdown;
-	
+
+	// String path_to_extension =
+	// "C:\\Users\\Angie\\AppData\\Local\\Google\\Chrome\\User
+	// Data\\Default\\Extensions\\cfhdojbkjhnklbpkdaibdccddilifddb\\3.5_0";
+	// ChromeOptions chrome_options;
 
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 
+		// this.chrome_options = new ChromeOptions();
+		// this.chrome_options.addArguments("load-extension=" + path_to_extension);
+		// DesiredCapabilities capabilities = new DesiredCapabilities();
+		// capabilities.setCapability(ChromeOptions.CAPABILITY, this.chrome_options);
+
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+		// this.driver = new ChromeDriver(capabilities);
 		this.driver = new ChromeDriver();
-		this.driver.get("https://es.calcuworld.com/calculadoras-financieras/calculadora-iva/");
-
-		this.buttonSinIva = this.driver.findElement(By.id("btn-one"));
-		this.camponum1 = this.driver.findElement(By.name("numero"));
-		this.selectTipo = this.driver.findElement(By.name("porcentaje"));
-		this.buttonCalcular = this.driver.findElement(By.xpath("//*[@id=\"post-7\"]/div/div[3]/form/input[2]"));
-        this.dropdown =new Select(this.selectTipo);	
-        
-        dropdown.selectByVisibleText("IVA General (21%)");
-        //dropdown.selectByIndex(2);
 		
+		this.driver.get("https://es.calcuworld.com/calculadoras-financieras/calculadora-iva/");
+		driver.manage().window().maximize();
+
+		// this.buttonConIva = this.driver.findElement(By.xpath("//*[@id=\"post-7\"]/div/div[3]/div[2]/ul/li[1]/a"));
+		// this.buttonSinIva = this.driver.findElement(By.xpath("//ul[@class='nav nav-tabs']/li[2]"));
+		this.buttonSinIva = (new WebDriverWait(this.driver, 5)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul[@class='nav nav-tabs']/li[2]")));
+		this.camponum = this.driver.findElement(By.xpath("//*[@id=\"profile\"]/form/div/div/input"));
+		this.selectTipo = this.driver.findElement(By.xpath("//*[@id=\"profile\"]/form/select"));
+		this.dropdown = new Select(this.selectTipo);
+		this.buttonCalcular = this.driver.findElement(By.xpath("//*[@id=\"profile\"]/form/button"));
+
+
+	}
+
+	@Test
+	public void testSinIva() throws InterruptedException {
+		this.buttonSinIva.click();
+		this.camponum.sendKeys("75");
+		this.dropdown.selectByIndex(1);
+		// dropdown.selectByVisibleText("IVA General (21%)");
+		this.buttonCalcular.click();
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	}
+		this.driver.quit();
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
